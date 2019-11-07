2202af0ae62ae2d03f41240108ce417f15705a7d
p.kratochvil <p.kratochvil@gmc.net>
2019-11-07 14:48:35 +0100

    few improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import * as b from "bobril";
 
+type BoxedTypes = string | undefined | null | number | boolean | Function;
+
+type Box<T> = {
+    value: T
+}
+
 export interface IHookRender<T, P extends any[]> {
     bobrilNode: {current: b.IBobrilCacheNode};
     element: Element;
-    currentValue: T
+    timesRerendered: () => number;
+    currentValue: T extends BoxedTypes ? Box<T> : T;
     changeDependencies(...dependencies: P): void;
 }
 
@@ -12,13 +19,16 @@ export function renderHook<T, P extends any[]>(hook: (...args: P) => T, ...depen
     return renderHookInsideParent(hook, null, ...dependencies);
 }
 
-export function renderHookInsideParent<T, P extends any[]>(hook: (...args: P) => T, Parent: b.IComponentFactory<any> | null,  ...dependencies: P) {
+export function renderHookInsideParent<T, P extends any[]>(hook: (...args: P) => T, Parent: b.IComponentFactory<any> | null,  ...dependencies: P): IHookRender<T, P> {
     let currentValue: T = {} as T;
     let deps = dependencies;
+    let timesRerendered: number = 0;
     let cacheNode: {current: b.IBobrilCacheNode} = {current: null};
     let domNode: HTMLDivElement;
     function Component() {
-        Object.assign(currentValue, hook(...deps));
+        const result = hook(...deps);
+        Object.assign(currentValue, typeof result === "object" ? result : {value: result});
+        timesRerendered++;
         return <div ref={cacheNode}>test</div>
     }
 
@@ -29,18 +39,15 @@ export function renderHookInsideParent<T, P extends any[]>(hook: (...args: P) =>
     domNode = b.getDomNode(cacheNode.current) as HTMLDivElement;
 
     return {
-        currentValue,
+        currentValue: currentValue as any,
         element: domNode,
         bobrilNode: cacheNode,
+        timesRerendered: () => timesRerendered,
         changeDependencies(...dependencies: P) {
             deps = dependencies;
             rerender();
         }
     }
-}
-
-export function afterEffect() {
-    return new Promise(resolve => b.asap(resolve));
 }
 
 export function rerender() {
